3a18a6de1bb4b5500e94760e46f32d839b9d46b1
zhijieeeeee <tang562250920@gmail.com>
2016-11-03 13:37:08 +0800

    add preview

--- app/src/main/java/com/don/clockview/MainActivity.java
+++ app/src/main/java/com/don/clockview/MainActivity.java
@@ -2,12 +2,25 @@ package com.don.clockview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.don.clockviewlibrary.ClockView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ClockView clockView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clockView = (ClockView) findViewById(R.id.clockView);
+        clockView.setOnCurrentTimeListener(new ClockView.OnCurrentTimeListener() {
+            @Override
+            public void currentTime(String time) {
+                Log.i("MyLog", time);
+            }
+        });
     }
 }
